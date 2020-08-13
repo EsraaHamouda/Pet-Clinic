a268420adb68f67fe8e2fd06c2178a7df801d9f1
EsraaHamouda <esraahamouda94@gmail.com>
2020-08-13 22:11:42 +0200

    project initialization

---
+++ clinic-pet/src/test/java/com/springframework/clinic/pet/ClinicPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.clinic.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
